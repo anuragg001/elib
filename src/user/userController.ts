1950c0d9224689f1bfb3b86ddb4c706c4beaadaa
anurag jaiswal <anuraggjaiswal@gmail.com>
2026-07-29 23:39:19 +0530

    userContoller and userCreation

--- src/user/userController.ts
+++ src/user/userController.ts
@@ -1,6 +1,7 @@
 import { NextFunction, Request, Response } from "express";
 import createHttpError from "http-errors";
 import userModel from "./userModel";
+import bcrypt from "bcrypt";
 
 const createUser = async (req: Request, res: Response, next: NextFunction) => {
 
@@ -13,26 +14,27 @@ const createUser = async (req: Request, res: Response, next: NextFunction) => {
     }
 
     //db call
+    //process the request to create a new user
     const user = await userModel.findOne({ email: email });
 
     if(user){
         const error = createHttpError(409, "User with this email already exists");
         return next(error);
     }
-    
+    // now store the user 
+    const hashedPassword = await bcrypt.hash(password,10)
 
+    const newUser = await userModel.create({
+        name,
+        email,
+        password: hashedPassword
+    })
 
-    //process the request to create a new user
+    // token generation logic can be added here if needed JWT
 
 
     //response
-
-
-
-
-
-    // Implementation for creating a new user
-    res.json({ message: "User registered successfully" })
+    res.json({id: newUser._id});
 };
 
 export { createUser };
