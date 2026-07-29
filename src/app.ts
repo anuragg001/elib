13c0bf62c93472092fda7c26a89d0e984e5ee363
anurag jaiswal <anuraggjaiswal@gmail.com>
2026-07-29 11:20:51 +0530

    globalErrorhandler

--- src/app.ts
+++ src/app.ts
@@ -1,11 +1,16 @@
 import express from "express";
+import globalErrorHandler from "./middlewares/globalErrorHandler";
 
 const app = express();
 
 // Routes
-
 app.get("/", (req, res, next) => {
+   
     res.json({ message: "Welcome to the API" });
 })
+
+
+// global error handler 
+app.use(globalErrorHandler);
 
 export default app;
