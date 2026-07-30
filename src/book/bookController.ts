b65361dc61bd2f8b574bfb86418740e813254e84
anurag jaiswal <anuraggjaiswal@gmail.com>
2026-07-30 16:33:42 +0530

    add Book Model

---
+++ src/book/bookController.ts
@@ -0,0 +1,14 @@
+import { NextFunction, Request, Response } from "express";
+
+const createBook = async (req: Request, res: Response, next: NextFunction) => {
+
+
+    
+
+
+
+
+    res.json({ message: "Book created successfully" });
+}
+
+export { createBook };
