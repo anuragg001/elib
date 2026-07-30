27e1702abd8fa29bc48f62bcdce1ab9a0af93da9
anurag jaiswal <anuraggjaiswal@gmail.com>
2026-07-30 16:39:28 +0530

    add multer to handle formdata

--- src/book/bookController.ts
+++ src/book/bookController.ts
@@ -2,8 +2,8 @@ import { NextFunction, Request, Response } from "express";
 
 const createBook = async (req: Request, res: Response, next: NextFunction) => {
 
+    //get the data  i.e formData  (we already handled by multer in the router)
 
-    
 
 
 
--- src/book/bookRouter.ts
+++ src/book/bookRouter.ts
@@ -1,10 +1,23 @@
 import express from "express";
 import { createBook } from "./bookController";
+import multer from "multer";
+import path from "node:path";
 
 const bookRouter = express.Router();
 
+// file store local -->  temporily store the file in the server and then move it to the public folder
+const upload = multer({
+    dest: path.resolve(__dirname, '../../public/data/uploads'),
+    limits: { fileSize: 10 * 1024 * 1024 } //10MB
+})
+
+
 //routes
-bookRouter.post('/', createBook);
+bookRouter.post('/', upload.fields([
+    { name: 'coverImage', maxCount: 1 },
+    { name: 'file', maxCount: 1 },
+
+]), createBook);
 
 
 export default bookRouter;
